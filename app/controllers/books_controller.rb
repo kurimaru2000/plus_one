a42c62a253ab9ce1737817a5373c372ba1024089
kurimaru2000 <na11012000@gmail.com>
2021-04-07 17:13:29 +0900

    投稿機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,19 @@ class BooksController < ApplicationController
 def index
 end
 
+def new
+  @book = Book.new
+end
+
+def create
+  Book.create(book_params)
+  redirect_to root_path
+end
+
+end
+
+private
+
+def book_params
+    params.require(:book).permit(:title, :author, :publishing_company, :explanation, :image).merge(user_id: current_user.id)
 end
