1da9bda53fe9aaaffb7b1ec8643efafedbfd3980
kurimaru2000 <na11012000@gmail.com>
2021-04-20 19:36:15 +0900

    デバッグ検証　フッター

---
+++ config/deploy.rb
@@ -0,0 +1,33 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.16.0"
+
+
+set :application, "plus_one"
+
+
+set :repo_url, 'github.com:kurimaru2000/plus_one.git'
+
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、’2.6.5’ です
+
+# どの公開鍵を利用してデプロイするか
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/kurimaru2000.kurimaru2000.cer'] 
+
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
